3e05ff86034c07cfc609314e2bfcad392c218211
Flow-github <ideation@hotmail.fr>
2022-03-23 16:55:04 +0100

    create global component + content of the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { StructureComponent } from './features/structure/structure.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainHeaderComponent } from './shared/components/main-header/main-header.component';
+import { MainMenuComponent } from './shared/components/main-menu/main-menu.component';
+import { MainFooterComponent } from './shared/components/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    StructureComponent
+    StructureComponent,
+    MainHeaderComponent,
+    MainMenuComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
